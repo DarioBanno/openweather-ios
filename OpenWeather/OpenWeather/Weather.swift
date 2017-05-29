86446c1fa4b0490ef41ad9a2906aefbff23058fe
Dario <dario.banno@apptown.io>
2017-05-29 15:45:01 +0100

    Implemented Weather JSON Serialization + unit tests

--- OpenWeather/OpenWeather/Weather.swift
+++ OpenWeather/OpenWeather/Weather.swift
@@ -20,3 +20,38 @@ struct Weather {
     var temperatureMax: Double
 
 }
+
+
+extension Weather: JSONSerializable {
+    
+    init?(json: JSONObject?) {
+        guard let weatherResults = json?["weather"] as? [JSONObject],
+            let weather = weatherResults.first,
+            let code = weather["id"] as? Int,
+            let description = weather["main"] as? String,
+            let wind = Wind(json: json?["wind"] as? JSONObject),
+            let timestamp = json?["dt"] as? TimeInterval
+            else {
+                return nil
+        }
+        
+        self.code = code
+        self.description = description
+        self.wind = wind
+        self.timestamp = timestamp
+        
+        guard let main = json?["main"] as? JSONObject,
+            let temperature = main["temp"] as? Double,
+            let temperatureMin = main["temp_min"] as? Double,
+            let temperatureMax = main["temp_max"] as? Double,
+            let humidity = main["humidity"] as? Int else {
+                return nil
+        }
+        
+        self.temperature = temperature
+        self.temperatureMin = temperatureMin
+        self.temperatureMax = temperatureMax
+        self.humidity = humidity
+    }
+    
+}
--- OpenWeather/OpenWeatherTests/WeatherTests.swift
+++ OpenWeather/OpenWeatherTests/WeatherTests.swift
@@ -13,11 +13,37 @@ class WeatherTests: XCTestCase {
     
 
     func test_invalid_json_population() {
+        // GIVEN I have an invalid json
+        let jsonObject = ["ciao": "bella"]
         
+        // WHEN I populate a weather object from json
+        let weather = Weather(json: jsonObject)
+        
+        // THEN I can see object is nil
+        XCTAssertNil(weather)
     }
     
+    
     func test_valid_json_population() {
-
+        // GIVEN I have a json object for weather
+        let jsonObject = json(forFile: "weather.json")!
+        
+        // WHEN I populate a weather object from json
+        let weather = Weather(json: jsonObject)
+        
+        // THEN I can see object is not nil
+        XCTAssertNotNil(weather)
+        
+        // AND I can see all fields are properly populated
+        XCTAssertEqual(weather!.code, 803)
+        XCTAssertEqual(weather!.description, "Clouds")
+        XCTAssertEqual(weather!.humidity, 78)
+        XCTAssertEqual(weather!.temperature, 287.11)
+        XCTAssertEqual(weather!.temperatureMin, 287.11)
+        XCTAssertEqual(weather!.temperatureMax, 289.755)
+        XCTAssertEqual(weather!.timestamp, 1496026800)
+        XCTAssertEqual(weather!.wind.speed, 3.46)
+        XCTAssertEqual(weather!.wind.degrees, 85.5016)
     }
     
 }
